564b502747adc7c3870945087becb999e6abddf3
John Else <john.else@citrix.com>
2016-02-03 03:11:14 +0000

    Bind enough properties and methods to create a basic oscillator
    Signed-off-by: John Else <john.else@citrix.com>

--- lib/webAudio.mli
+++ lib/webAudio.mli
@@ -1,6 +1,37 @@
-class type audioContext = object
+class type audioParam = object
+  method value : float Js.prop
+  method defaultValue : float Js.readonly_prop
+end
+
+class type audioNode = object
+  method context : audioContext Js.t Js.readonly_prop
+  method numberOfInputs : int Js.readonly_prop
+  method numberOfOutputs : int Js.readonly_prop
+
+  method connect : audioNode Js.t -> unit Js.meth
+  method disconnect : unit Js.meth
+end
+
+and audioDestinationNode = object
+  inherit audioNode
+end
+
+and oscillatorNode = object
+  inherit audioNode
+
+  method frequency : audioParam Js.t Js.prop
+  method _type : Js.js_string Js.t Js.prop
+
+  method start : unit Js.meth
+  method stop : unit Js.meth
+end
+
+and audioContext = object
   method currentTime : int Js.readonly_prop
+  method destination : audioDestinationNode Js.t Js.readonly_prop
   method sampleRate : float Js.readonly_prop
+
+  method createOscillator : oscillatorNode Js.t Js.meth
 end
 
 val audioContext : audioContext Js.t Js.constr
