58a2b741776e64b6307b9652e0f2f9026cf81fc3
John Else <john.else@gmail.com>
2016-03-06 00:55:16 +0000

    test: Print a summary of the results
    Signed-off-by: John Else <john.else@gmail.com>

--- test/test_suite.ml
+++ test/test_suite.ml
@@ -44,6 +44,18 @@ let suite =
 
 let run_suite log =
   let open T in
-  let (_ : result list) = run log suite in
-  ()
+  let results = run log suite in
+  let total, errored, failed, succeeded =
+    List.fold_left
+      (fun (total, errors, failures, successes) result ->
+        match result with
+        | Error _ -> total + 1, errors + 1, failures, successes
+        | Failure _ -> total + 1, errors, failures + 1, successes
+        | Success -> total + 1, errors, failures, successes + 1)
+      (0, 0, 0, 0) results
+  in
+  log (Printf.sprintf "%d tests run" total);
+  log (Printf.sprintf "%d errors" errored);
+  log (Printf.sprintf "%d failures" failed);
+  log (Printf.sprintf "%d succeeded" succeeded)
 
