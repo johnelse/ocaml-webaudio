844abd3df7165319bd7ce27ba8f58f717e90e75f
John Else <john.else@gmail.com>
2021-07-04 01:31:23 +0100

    test_driver.py: Fix path to html document

--- test/helpers/test_driver.py
+++ test/helpers/test_driver.py
@@ -17,7 +17,7 @@ def main():
     """
     driver = webdriver.Firefox()
     driver.get(
-        "file://%s" % (os.path.join(os.getcwd(), "test/test_runner.html")))
+        "file://%s" % (os.path.join(os.getcwd(), "helpers/test_runner.html")))
 
     WebDriverWait(driver, 10).until(
         lambda driver:
